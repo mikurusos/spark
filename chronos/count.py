619080cf1fa8fe32f68d1a7771a2ee6965491e99
oasis2811623 <oasis2811@gmail.com>
2016-03-02 20:02:47 +0800

    update

--- chronos/count.py
+++ chronos/count.py
@@ -1,23 +1,34 @@
 # -*- coding: utf-8 -*-
 import sys
 import json
-import math
+import cPickle as pickle
 from operator import add
 from pyspark import SQLContext, SparkContext, SparkConf
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-
-keyword='book'
+try:
+    print "loading..."
+    with open("gender.pkl", "rb") as f:
+        gender = pickle.load(f)
+    print "finished!"
+except:
+    gender = {}
 
 conf = SparkConf().setAppName("chencheng's task").setMaster("spark://anti-spam-spark-001.yz.momo.com:8081,anti-spam-spark-002.yz.momo.com:8081")
 sc = SparkContext(conf=conf)
 
+b = sc.broadcast(gender)
+
 data = sc.textFile("hdfs://antispam/user/hadoop/output/wang.yuqi/Venus/like_person/2016030218-24/")
 
-output = data.map(lambda x : x.split('\t')).flatMap(lambda x: json.loads(json.loads(x[0]))) \
-        .map(lambda x: (x,1)).reduceByKey(lambda x,y:x).collect()
+tmp = data.map(lambda x : x.split('\t')).map(lambda x: (json.loads(json.loads(x[0])), int(x[1][0])))
 
-with open('/home/hadoop/chen.cheng/Chronos/0302_count', 'w') as f:
-    for item in output:
-        f.write("%s\n" %( item[0]  ) )
+tmp.cache()
+
+male = tmp.filter(lambda x: gender[int(x[1][1])]=='M').count()
+
+female = tmp.filter(lambda x: gender[int(x[1][1])]=='F').count()
+
+with open('/home/hadoop/chen.cheng/Chronos/0302_genderNum', 'w') as f:
+    f.write("%d\t%d" %( female, male  ) )
