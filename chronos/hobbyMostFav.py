4e492c65cfd94d60e5197c8eae57118a5dca09ba
oasis2811623 <oasis2811@gmail.com>
2016-03-02 16:27:58 +0800

    update

--- chronos/hobbyMostFav.py
+++ chronos/hobbyMostFav.py
@@ -15,7 +15,7 @@ def flat(l):
     return data
 
 
-keyword='music'
+keyword='movie'
 
 conf = SparkConf().setAppName("chencheng's task").setMaster("spark://anti-spam-spark-001.yz.momo.com:8081,anti-spam-spark-002.yz.momo.com:8081")
 sc = SparkContext(conf=conf)
