ba40ee100ab09ab60e54bb7648952caa8a5e12aa
oasis2811623 <oasis2811@gmail.com>
2016-03-03 14:12:13 +0800

    update

--- chronos/successPair.py
+++ chronos/successPair.py
@@ -25,15 +25,15 @@ b = sc.broadcast(gender)
 tmp = data.map(lambda x : x.split('\t')).map(lambda x: (json.loads(json.loads(x[0])), json.loads(x[1])[0]))\
         .filter(lambda x: x[1]).map(lambda x: sorted([int(x[0][0]), int(x[0][1])]))\
         .map(lambda x:(tuple(x),1)).reduceByKey(lambda x,y:x)
-tmp.cache()
 
 #dateNum = tmp.map(lambda x:(tuple(x),1)).reduceByKey(lambda x,y:x).count()
 
 success = tmp.flatMap(lambda x :list(x[0])).map(lambda x:(x,1)).reduceByKey(lambda x,y:x)
+success.cache()
 
-male = success.filter(lambda x: x[0] in gender and gender[x]=='M').count()
+male = success.filter(lambda x: x[0] in gender and gender[x[0]]=='M').count()
 
-female = success.filter(lambda x: x[0] in gender and gender[x]=='F').count()
+female = success.filter(lambda x: x[0] in gender and gender[x[0]]=='F').count()
 
 with open('/home/hadoop/chen.cheng/Chronos/0302_successGenderNum', 'w') as f:
     f.write("%d\t%d" %(female, male  ) )
