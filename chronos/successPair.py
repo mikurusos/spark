391836f743acecbdf4e360be1dbbe70e4c48199e
oasis2811623 <oasis2811@gmail.com>
2016-03-03 12:25:28 +0800

    update

--- chronos/successPair.py
+++ chronos/successPair.py
@@ -12,10 +12,13 @@ sc = SparkContext(conf=conf)
 
 data = sc.textFile("hdfs://antispam/user/hadoop/output/wang.yuqi/Venus/like_person/2016030218-24/")
 
-output = data.map(lambda x : x.split('\t')).map(lambda x: (json.loads(json.loads(x[0])), json.loads(x[1])[0]))\
-        .filter(lambda x: x[1]).map(lambda x: tuple(sorted([int(x[0][0]), int(x[0][1])])))\
-        .map(lambda x:(x,1)).reduceByKey(lambda x,y:x).count()
+tmp = data.map(lambda x : x.split('\t')).map(lambda x: (json.loads(json.loads(x[0])), json.loads(x[1])[0]))\
+        .filter(lambda x: x[1]).map(lambda x: sorted([int(x[0][0]), int(x[0][1])]))
+tmp.cache()
 
+dataNum = tmp.map(lambda x:(tuple(x),1)).reduceByKey(lambda x,y:x).count()
+
+pNum = tmp.flatMap(lambda x :x).map(lambda x:(tuple(x),1)).reduceByKey(lambda x,y:x).count()
 
 with open('/home/hadoop/chen.cheng/Chronos/0302_successNum', 'w') as f:
-    f.write("%d" %( output  ) )
+    f.write("%d\t%d" %( dataNum, pNum  ) )
