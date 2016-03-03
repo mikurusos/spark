fc679b6bce4874b6f840867925eb994967ad422f
oasis2811623 <oasis2811@gmail.com>
2016-03-03 14:41:27 +0800

    update

---
+++ chronos/rank.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import sys
+import json
+import cPickle as pickle
+from operator import add
+from pyspark import SQLContext, SparkContext, SparkConf
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+conf = SparkConf().setAppName("chencheng's task").setMaster("spark://anti-spam-spark-001.yz.momo.com:8081,anti-spam-spark-002.yz.momo.com:8081")
+sc = SparkContext(conf=conf)
+
+data = sc.textFile("hdfs://antispam/user/hadoop/output/wang.yuqi/Venus/like_person/2016030218-24/")
+
+out = data.map(lambda x : x.split('\t')).map(lambda x: (json.loads(json.loads(x[0])), json.loads(x[1])[0]))\
+        .filter(lambda x: x[1]).map(lambda x: (int(x[0][1]), 1))\
+        .reduceByKey(add).collect()
+
+with open('/home/hadoop/chen.cheng/Chronos/0302_successGenderNum', 'w') as f:
+    f.write("%d\t%d" %(out[0], out[1]  ) )
