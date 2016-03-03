81995b2f388f9cf71de80749577beb262e7a9619
oasis2811623 <oasis2811@gmail.com>
2016-03-03 12:43:50 +0800

    update

--- chronos/successPair.py
+++ chronos/successPair.py
@@ -12,6 +12,16 @@ sc = SparkContext(conf=conf)
 
 data = sc.textFile("hdfs://antispam/user/hadoop/output/wang.yuqi/Venus/like_person/2016030218-24/")
 
+try:
+    print "loading..."
+    with open("/home/hadoop/chen.cheng/moa/gender.pkl", "rb") as f:
+        gender = pickle.load(f)
+    print "finished!"
+except:
+    gender = {}
+
+b = sc.broadcast(gender)
+
 tmp = data.map(lambda x : x.split('\t')).map(lambda x: (json.loads(json.loads(x[0])), json.loads(x[1])[0]))\
         .filter(lambda x: x[1]).map(lambda x: sorted([int(x[0][0]), int(x[0][1])]))\
         .map(lambda x:(tuple(x),1)).reduceByKey(lambda x,y:x)
@@ -19,7 +29,11 @@ tmp.cache()
 
 #dateNum = tmp.map(lambda x:(tuple(x),1)).reduceByKey(lambda x,y:x).count()
 
-pNum = tmp.flatMap(lambda x :list(x[0])).map(lambda x:(x,1)).reduceByKey(lambda x,y:x).count()
+success = tmp.flatMap(lambda x :list(x[0])).map(lambda x:(x,1)).reduceByKey(lambda x,y:x)
 
-with open('/home/hadoop/chen.cheng/Chronos/0302_successNum', 'w') as f:
-    f.write("%d" %(pNum  ) )
+male = success.filter(lambda x: int(x[0][0]) in gender and gender[int(x[0][0])]=='M').count()
+
+female = success.filter(lambda x: int(x[0][0]) in gender and gender[int(x[0][0])]=='F').count()
+
+with open('/home/hadoop/chen.cheng/Chronos/0302_successGenderNum', 'w') as f:
+    f.write("%d\t%d" %(female, male  ) )
