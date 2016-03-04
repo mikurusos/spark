b511cff3926266038a3cddaad1bd12f563a7bc3b
oasis2811623 <oasis2811@gmail.com>
2016-03-04 17:13:01 +0800

    update

--- crux/als_wr.py
+++ crux/als_wr.py
@@ -1,5 +1,6 @@
 import sys
 import json
+from util import loadPickle
 from operator import add
 from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
 from pyspark import SQLContext, SparkContext, SparkConf
@@ -11,11 +12,13 @@ sc = SparkContext(conf=conf)
 
 user_artist_data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/bobby/book/")
 
+tfidf=loadPickle('/home/hadoop/chen.cheng/moa/book_tfidf.pkl')
+#b = sc.broadcast(tfidf)
 
 ratings = user_artist_data.map(lambda x: json.loads(x))\
         .flatMap(lambda x: [[x[0], item] for item in x[1]]) \
         .filter(lambda x: x[0] and x[1] )\
-        .map(lambda x: Rating(int(x[0]), int(x[1]), 1))
+        .map(lambda x: Rating(int(x[0]), int(x[1]), tfidf[x[0]]))
 ratings.cache()
 
 rank = 10
