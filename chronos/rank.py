8ad0dfcc87eeae61fc9d6b80f5609886c99ce3c1
oasis2811623 <oasis2811@gmail.com>
2016-03-04 10:19:24 +0800

    update

--- chronos/rank.py
+++ chronos/rank.py
@@ -10,12 +10,12 @@ sys.setdefaultencoding('utf-8')
 conf = SparkConf().setAppName("chencheng's task").setMaster("spark://anti-spam-spark-001.yz.momo.com:8081,anti-spam-spark-002.yz.momo.com:8081")
 sc = SparkContext(conf=conf)
 
-data = sc.textFile("hdfs://antispam/user/hadoop/output/wang.yuqi/Venus/like_person/2016030218-24/")
+data = sc.textFile("hdfs://antispam/user/hadoop/output/wang.yuqi/Venus/like_person/2016030318-24/")
 
 out = data.map(lambda x : x.split('\t')).map(lambda x: (json.loads(json.loads(x[0])), json.loads(x[1])[0]))\
         .filter(lambda x:  x[1]==0).map(lambda x: (int(x[0][1]), 1))\
         .reduceByKey(add).collect()
 
-with open('/home/hadoop/chen.cheng/Chronos/0302_unlikeRank', 'w') as f:
+with open('/home/hadoop/chen.cheng/Chronos/0303_unlikeRank', 'w') as f:
     for item in out:
         f.write("%d\t%d\n" %(item[0], item[1]  ) )
