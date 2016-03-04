5c1236777349a64833c1803efba347b1871610eb
oasis2811623 <oasis2811@gmail.com>
2016-03-04 16:35:05 +0800

    update

--- crux/als_wr.py
+++ crux/als_wr.py
@@ -9,16 +9,16 @@ sys.setdefaultencoding('utf-8')
 conf = SparkConf().setAppName("chencheng's task").setMaster("spark://anti-spam-spark-001.yz.momo.com:8081,anti-spam-spark-002.yz.momo.com:8081")
 sc = SparkContext(conf=conf)
 
-user_artist_data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/2016030218-24_male/")
+user_artist_data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/bobby/book/")
 
 
 ratings = user_artist_data.map(lambda x: json.loads(x))\
-        .filter(lambda x: x[0][0] and x[0][1])\
-        .map(lambda x: Rating(int(x[0][0]), int(x[0][1]), float(x[1])))
+        .flatMap(lambda x: [[x[0], item] for item in x[1]]) \
+        .map(lambda x: Rating(int(x[0][0]), int(x[0][1]), 1))
 ratings.cache()
 
 rank = 10
 numIterations = 10
 model = ALS.trainImplicit(ratings, rank, numIterations, alpha=0.01)
 
-model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_male2female")
+model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_book")
