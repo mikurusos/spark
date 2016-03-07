9310ade0183fac8359043a03c43c64c7b7c9a3d9
oasis2811623 <oasis2811@gmail.com>
2016-03-07 10:34:40 +0800

    update

--- crux/als_wr.py
+++ crux/als_wr.py
@@ -21,8 +21,8 @@ ratings = user_artist_data.map(lambda x: json.loads(x))\
         .map(lambda x: Rating(int(x[0]), int(x[1]), 1))
 ratings.cache()
 
-rank = 20
-numIterations = 30
+rank = 10
+numIterations = 20
 model = ALS.trainImplicit(ratings, rank, numIterations, alpha=0.01)
 
 model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_book")
