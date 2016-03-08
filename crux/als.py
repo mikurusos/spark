179d404a2e01251ed9b9ead83acb15b5920a7662
oasis2811623 <oasis2811@gmail.com>
2016-03-08 19:13:27 +0800

    update

--- crux/als.py
+++ crux/als.py
@@ -9,7 +9,7 @@ sys.setdefaultencoding('utf-8')
 conf = SparkConf().setAppName("chencheng's task").setMaster("spark://anti-spam-spark-001.yz.momo.com:8081,anti-spam-spark-002.yz.momo.com:8081")
 sc = SparkContext(conf=conf)
 
-user_artist_data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/male/*/")
+user_artist_data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/male/2016030[5-7]18/")
 
 ratings = user_artist_data.map(lambda x: json.loads(x))\
         .filter(lambda x: x[0][0] and x[0][1] and x[1])\
