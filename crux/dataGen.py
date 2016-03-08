cb4308c14a5eb14e8420aa30ba8011c1d28f17a9
oasis2811623 <oasis2811@gmail.com>
2016-03-08 11:49:37 +0800

    update

--- crux/dataGen.py
+++ crux/dataGen.py
@@ -7,12 +7,11 @@ sys.setdefaultencoding('utf-8')
 from config import sc
 
 
-data = sc.textFile("hdfs://antispam/user/hadoop/output/wang.yuqi/Venus/like_person/2016030418-24/")
+data = sc.textFile("hdfs://antispam/user/hadoop/output/wang.yuqi/Venus/like_person/%s-24/" %(sys.argv[1]))
 
 out = data.map(lambda x : x.split('\t')).map(lambda x: [json.loads(json.loads(x[0])), json.loads(x[1])[0]])\
         .map(lambda x: json.dumps(x))
 
-out.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/rawData/2016030418-24/")
-
+out.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/rawData/%s-24/"%(sys.argv[1]))
 
 sc.stop()
