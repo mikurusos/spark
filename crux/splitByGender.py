0742fc588358f1e56b53c052f33fed8333b7fb1c
oasis2811623 <oasis2811@gmail.com>
2016-03-08 18:21:19 +0800

    update

--- crux/splitByGender.py
+++ crux/splitByGender.py
@@ -8,9 +8,11 @@ sys.setdefaultencoding('utf-8')
 from config import sc
 from util import loadPickle
 
+date = sys.argv[1]
+
 gender = loadPickle("/home/hadoop/chen.cheng/moa/gender6.pkl")
 b = sc.broadcast(gender)
-data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/rawData/2016030518-24/")
+data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/rawData/%s-24/" % (date))
 
 out_male = data.map(lambda x : json.loads(x)).filter(lambda x: x[0][0] and x[0][1])\
     .filter(lambda x: b.value[int(x[0][0])] == "M").map(lambda x: json.dumps(x))
@@ -25,5 +27,5 @@ with open('/home/hadoop/chen.cheng/Chronos/momoid', 'w') as f:
         f.write("%s\n" %(item ) )
 '''
 
-out_male.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/male/2016030518/")
-out_female.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016030518/")
+out_male.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/male/%s/"% (date))
+out_female.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/%s/"% (date))
