7cc572661964b371ce962422f9a23bd85319c757
oasis2811623 <oasis2811@gmail.com>
2016-03-08 18:49:13 +0800

    update

--- crux/als.py
+++ crux/als.py
@@ -12,7 +12,7 @@ sc = SparkContext(conf=conf)
 user_artist_data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/male/*/")
 
 ratings = user_artist_data.map(lambda x: json.loads(x))\
-        .filter(lambda x: x[0][0] and x[0][1])\
+        .filter(lambda x: x[0][0] and x[0][1] and x[1])\
         .map(lambda x: Rating(int(x[0][0]), int(x[0][1]), float(x[1])))
 ratings.cache()
 
