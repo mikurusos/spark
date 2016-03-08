005be3f026529c1a3eca78679faa80c3d6c583e5
oasis2811623 <oasis2811@gmail.com>
2016-03-08 11:57:56 +0800

    update

---
+++ crux/sqlData.py
@@ -0,0 +1,20 @@
+import sys
+import json
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+from config import sc
+
+from pyspark.sql import SQLContext, Row
+sqlContext = SQLContext(sc)
+
+data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/rawData/%s-24/"%(sys.argv[1]))
+
+out = data.map(lambda x:json.loads(x)).map(lambda x: Row(sender=int(x[0][0]), receivor=int(x[0][1]), like=x[1]))
+
+schema = sqlContext.createDataFrame(out)
+schema.registerTempTable("people")
+
+schema.save("hdfs://antispam/user/hadoop/output/chencheng/crux/data/dataFrame/%s/"%(sys.argv[1]))
+
+sc.stop()
