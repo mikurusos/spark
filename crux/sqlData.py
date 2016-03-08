ac88d5a71bfbb3800168f188d8f196be9cf8ceec
oasis2811623 <oasis2811@gmail.com>
2016-03-08 12:04:14 +0800

    update

--- crux/sqlData.py
+++ crux/sqlData.py
@@ -10,7 +10,8 @@ sqlContext = SQLContext(sc)
 
 data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/rawData/%s-24/"%(sys.argv[1]))
 
-out = data.filter(lambda x: x[0][0] and x[0][1]).map(lambda x:json.loads(x)).map(lambda x: Row(sender=int(x[0][0]), receivor=int(x[0][1]), like=x[1]))
+out = data.map(lambda x:json.loads(x)).filter(lambda x: x[0][0] and x[0][1])\
+        .map(lambda x: Row(sender=int(x[0][0]), receivor=int(x[0][1]), like=x[1]))
 
 schema = sqlContext.createDataFrame(out)
 schema.registerTempTable("people")
