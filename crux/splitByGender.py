a7ce281b746f3e42b913c09df14a6e2c15a4b124
oasis2811623 <oasis2811@gmail.com>
2016-03-08 16:20:33 +0800

    update

--- crux/splitByGender.py
+++ crux/splitByGender.py
@@ -10,7 +10,7 @@ from util import loadPickle
 
 gender = loadPickle("/home/hadoop/chen.cheng/moa/gender4.pkl")
 b = sc.broadcast(gender)
-data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/rawData/2016030618-24/")
+data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/rawData/2016030518-24/")
 
 out_male = data.map(lambda x : json.loads(x)).filter(lambda x: x[0][0] and x[0][1])\
     .filter(lambda x: b.value[int(x[0][0])] == "M").map(lambda x: json.dumps(x))
@@ -25,5 +25,5 @@ with open('/home/hadoop/chen.cheng/Chronos/momoid', 'w') as f:
         f.write("%s\n" %(item ) )
 '''
 
-out_male.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/male/2016030618/")
-out_female.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016030618/")
+out_male.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/male/2016030518/")
+out_female.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016030518/")
