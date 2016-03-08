de67cd1d71c36490ced2a928cd0d094e282655a7
oasis2811623 <oasis2811@gmail.com>
2016-03-08 14:34:18 +0800

    update

--- crux/als.py
+++ crux/als.py
@@ -1,6 +1,5 @@
 import sys
 import json
-from crux.util import loadPickle
 from operator import add
 from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
 from pyspark import SQLContext, SparkContext, SparkConf
