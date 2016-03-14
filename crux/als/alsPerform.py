627d745a38333666421f104f0d2d91f7500bd658
oasis2811623 <oasis2811@gmail.com>
2016-03-14 12:13:12 +0800

    update

--- crux/als/alsPerform.py
+++ crux/als/alsPerform.py
@@ -9,6 +9,7 @@ sys.setdefaultencoding('utf-8')
 from crux.config import sc
 import os
 curPath = os.path.abspath(os.path.dirname(__file__))
+print curPath
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 
