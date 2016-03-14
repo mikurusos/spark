957425d9d2b5e4e62fd6c783672c53328eff3286
oasis2811623 <oasis2811@gmail.com>
2016-03-14 18:37:40 +0800

    update

--- crux/als/__init__.py
+++ crux/als/__init__.py
@@ -10,6 +10,7 @@ sys.setdefaultencoding('utf-8')
 conf = SparkConf().setAppName("chencheng's task").setMaster("spark://anti-spam-spark-001.yz.momo.com:8081,anti-spam-spark-002.yz.momo.com:8081")\
     .set("spark.driver.cores", 10)\
     .set("spark.driver.memory", "2g")\
+    .set("spark.dynamicAllocation.enabled","true")\
     .set("spark.serializer","org.apache.spark.serializer.KryoSerializer")\
     .set("spark.kryoserializer.buffer.mb", "1024")
 sc = SparkContext(conf=conf)
