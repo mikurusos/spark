1b86d9e0b1ce6f307779410d9ba2cda4e802932a
oasis2811623 <oasis2811@gmail.com>
2016-03-15 11:59:54 +0800

    update

--- crux/als/computeAUC.py
+++ crux/als/computeAUC.py
@@ -1,20 +1,33 @@
 from __init__ import *
 
-totalCount=sc.accumulator(0)
-invalidCount=sc.accumulator(0)
+totalTrue=sc.accumulator(0)
+totalFalse=sc.accumulator(0)
+invalidTrue=sc.accumulator(0)
+invalidFalse=sc.accumulator(0)
 
 def getThreshold(x):
-    global totalCount
-    totalCount+=1
-    return int(x[0]), 1 if(x[1]>0.55) else 0
+    global totalTrue, totalFalse
+    res = int(x[0]), 1 if(x[1]>0.55) else 0
+    if(res):
+        totalTrue+=1
+    else:
+        totalFalse+=1
+    return res
 
 def getInvalid(x):
-    global invalidCount
-    if(x[0]==x[1]):
-        invalidCount+=1
-        return 1
+    global invalidTrue, invalidFalse
+    if(x[0]==1):
+        if(x[0]==x[1]):
+            invalidTrue+=1
+            return 1
+        else:
+            return 0
     else:
-        return 0
+        if(x[0]==x[1]):
+            invalidFalse+=1
+            return 1
+        else:
+            return 0
 
 data = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/results/2016030918/")
 
@@ -22,4 +35,4 @@ data.map(lambda x:json.loads(x)).map(lambda x:(int(x[1][0]), x[1][1])).map(getTh
     .filter(getInvalid).count()
 
 with open('/home/hadoop/chen.cheng/Chronos/AUC', 'w') as f:
-    f.write("%d\t%d\n" %( totalCount.value, invalidCount.value ) )
+    f.write("%d\t%d\t%d\t%d\n" %( totalTrue.value, totalFalse.value, invalidTrue.value, invalidFalse.value ) )
