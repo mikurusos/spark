19249c95f82bee986e90314f7ae1c40fc73eb8e7
oasis2811623 <oasis2811@gmail.com>
2016-03-15 19:07:07 +0800

    update

--- crux/als.py
+++ crux/als.py
@@ -9,8 +9,8 @@ sys.setdefaultencoding('utf-8')
 conf = SparkConf().setAppName("chencheng's task").setMaster("spark://anti-spam-spark-001.yz.momo.com:8081,anti-spam-spark-002.yz.momo.com:8081")
 sc = SparkContext(conf=conf)
 
-user_artist_data1 = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016030[7-9]18/")
-user_artist_data2 = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016031018/")
+user_artist_data1 = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016030[8-9]18/")
+user_artist_data2 = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016031[0-1]18/")
 
 user_artist_data= user_artist_data1.union(user_artist_data2)
 
@@ -23,4 +23,4 @@ rank = 30
 numIterations = 20
 model = ALS.train(ratings, rank, numIterations)
 
-model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_female_0307-10")
+model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_female_0308-11")
