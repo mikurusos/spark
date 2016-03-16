e80a70cdd3a0e439c22b433270675966a129ba64
oasis2811623 <oasis2811@gmail.com>
2016-03-17 00:06:37 +0800

    update

--- crux/als/computeAUC.py
+++ crux/als/computeAUC.py
@@ -30,10 +30,10 @@ def getInvalid(x):
             return 0
 
 
-data = sc.textFile("%s/results/parameters/female/2016031518_005" % (HDFS_OUTPUT_PATH))
+data = sc.textFile("%s/results/parameters/female/2016031518_004" % (HDFS_OUTPUT_PATH))
 
 data.map(lambda x:json.loads(x)).map(lambda x:(int(x[1][0]), x[1][1])).map(getThreshold)\
     .filter(getInvalid).count()
 
-with open('/home/hadoop/chen.cheng/Chronos/parameters/AUC_2016031518_005', 'w') as f:
+with open('/home/hadoop/chen.cheng/Chronos/parameters/AUC_2016031518_004', 'w') as f:
     f.write("%d\t%d\t%d\t%d\n" %( totalTrue.value, totalFalse.value, invalidTrue.value, invalidFalse.value ) )
