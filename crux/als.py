991b6658b30ab10f0e14f2ef813cb262590cefbb
oasis2811623 <oasis2811@gmail.com>
2016-03-16 22:08:02 +0800

    update

--- crux/als.py
+++ crux/als.py
@@ -21,6 +21,6 @@ ratings.cache()
 
 rank = 30
 numIterations = 20
-model = ALS.train(ratings, rank, numIterations,lambda_=1)
+model = ALS.train(ratings, rank, numIterations,lambda_=1.0)
 
 model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_female_parameters/als_female_0311-14_30_100")
