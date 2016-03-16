288f9007a07b20ab3037aa36076ef299d21f683a
oasis2811623 <oasis2811@gmail.com>
2016-03-16 23:51:48 +0800

    update

--- crux/als/predictFuture.py
+++ crux/als/predictFuture.py
@@ -5,7 +5,7 @@ invalidCount=sc.accumulator(0)
 
 data = sc.textFile("%s/data/female/2016031518"  %(HDFS_OUTPUT_PATH) )
 
-model = MatrixFactorizationModel.load(sc,"%s/model/als_female_parameters/als_female_0311-14_30_003"  %(HDFS_HOME_PATH))
+model = MatrixFactorizationModel.load(sc,"%s/model/als_female_parameters/als_female_0311-14_30_005"  %(HDFS_HOME_PATH))
 
 data2predict=data.map(lambda x:json.loads(x)).filter(lambda x: x[0][0] and x[0][1])\
     .map(lambda x:((int(x[0][0]), int(x[0][1])),x[1]))
@@ -18,5 +18,5 @@ prediction = model.predictAll(data2predict.map(lambda x:x[0])).map(lambda x:((x.
 combins = data2predict.join(prediction).map(lambda x:(x,1))\
         .reduceByKey(lambda x,y:x).map(lambda x: json.dumps(x[0]))
 
-combins.saveAsTextFile("%s/results/parameters/female/2016031518_003"  %(HDFS_OUTPUT_PATH) )
+combins.saveAsTextFile("%s/results/parameters/female/2016031518_005"  %(HDFS_OUTPUT_PATH) )
 sc.stop()
