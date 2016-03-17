e22fe1e2bbbc25e058716b0012d1a52166318d29
oasis2811623 <oasis2811@gmail.com>
2016-03-17 10:17:55 +0800

    update

--- crux/als/comPurity.py
+++ crux/als/comPurity.py
@@ -18,5 +18,5 @@ data.map(lambda x:json.loads(x)).map(lambda x:(int(x[1][0]), x[1][1]))\
     .filter(lambda x:x[1]>int(sys.argv[1])).map(myCount).count()
 
 
-with open('/home/hadoop/chen.cheng/Chronos/parameters/AUC_2016031518_004', 'w') as f:
+with open('/home/hadoop/chen.cheng/Chronos/parameters/AUC_purity', 'w') as f:
     f.write("%d\t%d\n" %( like.value, dislike.value ) )
