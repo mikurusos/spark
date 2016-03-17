556d68e1e4ef4ea21fdfa7b40bdbde9137539f5a
oasis2811623 <oasis2811@gmail.com>
2016-03-17 10:20:22 +0800

    update

--- crux/als/comPurity.py
+++ crux/als/comPurity.py
@@ -1,5 +1,7 @@
-import sys
+# -*- coding: utf-8 -*-
 from __init__ import *
+
+threshold = int(sys.argv[1])
 
 like=sc.accumulator(0)
 dislike=sc.accumulator(0)
@@ -15,7 +17,7 @@ def myCount(x):
 data = sc.textFile("%s/results/parameters/female/2016031518_004" % (HDFS_OUTPUT_PATH))
 
 data.map(lambda x:json.loads(x)).map(lambda x:(int(x[1][0]), x[1][1]))\
-    .filter(lambda x:x[1]>int(sys.argv[1])).map(myCount).count()
+    .filter(lambda x:x[1]>threshold).map(myCount).count()
 
 
 with open('/home/hadoop/chen.cheng/Chronos/parameters/AUC_purity', 'w') as f:
