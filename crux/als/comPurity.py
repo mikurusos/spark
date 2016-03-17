f64f9a9df6e83049ac2864f3b256c299ef317548
oasis2811623 <oasis2811@gmail.com>
2016-03-17 10:57:33 +0800

    update

--- crux/als/comPurity.py
+++ crux/als/comPurity.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __init__ import *
+import numpy as np
 
 threshold = float(sys.argv[1])
 
+tmp_like=0
+tmp_dislike=0
 like=sc.accumulator(0)
 dislike=sc.accumulator(0)
 
@@ -14,11 +17,20 @@ def myCount(x):
         dislike+=1
     return x
 
+result={}
+
 data = sc.textFile("%s/results/parameters/female/2016031518_004" % (HDFS_OUTPUT_PATH))
 
-data.map(lambda x:json.loads(x)).map(lambda x:(int(x[1][0]), x[1][1]))\
-    .filter(lambda x:x[1]>threshold).map(myCount).count()
+tmp=data.map(lambda x:json.loads(x)).map(lambda x:(int(x[1][0]), x[1][1]))
+tmp.cache()
+
+for i in np.arange(0,1,0.1):
+    tmp.filter(lambda x:x[1]>i).map(myCount).count()
+    result[i] = (like.value - tmp_like, dislike.value- tmp_dislike)
+    tmp_like=like.value
+    tmp_dislike = dislike.value
 
 
 with open('/home/hadoop/chen.cheng/Chronos/parameters/AUC_purity', 'w') as f:
-    f.write("%d\t%d\n" %( like.value, dislike.value ) )
+    for item in result:
+        f.write("%d\t%d\t%d\n" %(item, result[item][0], result[item][1] ) )
