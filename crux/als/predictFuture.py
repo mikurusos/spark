f3d16d73584e2aa53c112041c2f1ed998da1c42e
oasis2811623 <oasis2811@gmail.com>
2016-03-17 23:15:42 +0800

    update

--- crux/als/predictFuture.py
+++ crux/als/predictFuture.py
@@ -10,14 +10,15 @@ data2predict.cache()
 
 #female1=data2predict.map(lambda x:(x[0][0],1)).reduceByKey(lambda x,y:x).count()
 
-female1 = data2predict.map(lambda x:(x,1)).reduceByKey(lambda x,y:x).count()
+#female1 = data2predict.map(lambda x:(x,1)).reduceByKey(lambda x,y:x).count()
 
 #predict the results
-prediction = model.predictAll(data2predict.map(lambda x:x[0]))
+prediction = model.predictAll(data2predict.map(lambda x:x[0])).map(lambda x:((x.user, x.product), x.rating))\
+    .map(lambda x:(x[0][1],1)).reduceByKey(lambda x,y:x).count()
 
 #female2 = prediction.reduceByKey(lambda x,y:x).count()
 
-female2= prediction.count()
+#female2= prediction.count()
 
 #combining with the real results
 #combins = data2predict.join(prediction).map(lambda x:(x,1))\
@@ -25,8 +26,8 @@ female2= prediction.count()
 #
 #combins.saveAsTextFile("%s/results/parameters/female/25/2016031518_003_"  %(HDFS_OUTPUT_PATH) )
 
-with open('/home/hadoop/chen.cheng/Chronos/female_2016031518_', 'w') as f:
-    f.write("%d\t%d" %(female1, female2))
+with open('/home/hadoop/chen.cheng/Chronos/female_2016031518_2', 'w') as f:
+    f.write("%d" %(prediction))
 
 
 sc.stop()
