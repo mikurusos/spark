501e8d7076fdf5999bc1927b6f00bf188bad2c1c
oasis2811623 <oasis2811@gmail.com>
2016-03-17 10:14:45 +0800

    update

---
+++ crux/als/comPurity.py
@@ -0,0 +1,21 @@
+from __init__ import *
+
+like=sc.accumulator(0)
+dislike=sc.accumulator(0)
+
+def myCount(x):
+    global like, dislike
+    if(x):
+        like+=1
+    else:
+        dislike+=1
+    return x
+
+data = sc.textFile("%s/results/parameters/female/2016031518_004" % (HDFS_OUTPUT_PATH))
+
+data.map(lambda x:json.loads(x)).map(lambda x:(int(x[1][0]), x[1][1]))\
+    .filter(lambda x:x[1]>0.8).map(myCount).count()
+
+
+with open('/home/hadoop/chen.cheng/Chronos/parameters/AUC_2016031518_004', 'w') as f:
+    f.write("%d\t%d\n" %( like.value, dislike.value ) )
