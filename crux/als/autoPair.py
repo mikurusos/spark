f6454b27385f2134d3d9699db425c333a4e29fbd
oasis2811623 <oasis2811@gmail.com>
2016-03-18 17:06:22 +0800

    update

--- crux/als/autoPair.py
+++ crux/als/autoPair.py
@@ -6,7 +6,7 @@ def pairAdd(x,y):
 
 female_results= sc.textFile("%s/data/testSample/female/20160315-1618_results_als_female_0311-14_30_003"  %(HDFS_OUTPUT_PATH) )
 
-male_results= sc.textFile("%s/data/testSample/male/20160315-1618_results_als_male_0311-14_30_003"  %(HDFS_OUTPUT_PATH) )
+male_results= sc.textFile("%s/data/testSample/male/20160315-1618_results_als_male_0312-14_30_003"  %(HDFS_OUTPUT_PATH) )
 
 femaleData= female_results.map(lambda x:json.loads(x))\
     .map(lambda x: ((x[0][1],x[0][0]),(1, (0,x[1][1])) ))
@@ -16,6 +16,6 @@ maleData= male_results.map(lambda x:json.loads(x))\
 
 pair = maleData.union(femaleData).reduceByKey(pairAdd).map(lambda x:json.loads(x))
 
-pair.saveAsTextFile("%s/data/testSample/pair/20160315-1618_results_als_female_0311-14_30_003"  %(HDFS_OUTPUT_PATH) )
+pair.saveAsTextFile("%s/data/testSample/pair/20160315-1618_results"  %(HDFS_OUTPUT_PATH) )
 
 sc.stop()
