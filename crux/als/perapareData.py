892f905b2e7865af850e942213e91365aa7f63e6
oasis2811623 <oasis2811@gmail.com>
2016-03-18 16:29:55 +0800

    update

--- crux/als/perapareData.py
+++ crux/als/perapareData.py
@@ -1,11 +1,11 @@
 import util
 from __init__ import *
 
-data1 = sc.textFile("%s/data/female/2016031518"  %(HDFS_OUTPUT_PATH) )
-data2 = sc.textFile("%s/data/female/2016031618"  %(HDFS_OUTPUT_PATH) )
+data1 = sc.textFile("%s/data/male/2016031518"  %(HDFS_OUTPUT_PATH) )
+data2 = sc.textFile("%s/data/male/2016031618"  %(HDFS_OUTPUT_PATH) )
 
 data= data1.union(data2)
 
-data.saveAsTextFile("%s/data/testSample/female/20160315-1618"  %(HDFS_OUTPUT_PATH) )
+data.saveAsTextFile("%s/data/testSample/male/20160315-1618"  %(HDFS_OUTPUT_PATH) )
 
 sc.stop()
