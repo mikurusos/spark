35a3eed969170ec08d818e108f02f6610f65d23a
oasis2811623 <oasis2811@gmail.com>
2016-03-18 16:40:21 +0800

    update

--- crux/als/predictFuture.py
+++ crux/als/predictFuture.py
@@ -1,7 +1,7 @@
 import util
 from __init__ import *
 
-data = sc.textFile("%s/data/male/2016031618"  %(HDFS_OUTPUT_PATH) )
+data = sc.textFile("%s/data/testSample/male/20160315-1618"  %(HDFS_OUTPUT_PATH) )
 
 model = MatrixFactorizationModel.load(sc,"%s/model/als_male_parameters/30/als_male_0312-14_003"  %(HDFS_HOME_PATH))
 
@@ -15,6 +15,6 @@ prediction = util.predictData(model,data2predict)
 combins = data2predict.join(prediction).map(lambda x:(x,1))\
         .reduceByKey(lambda x,y:x).map(lambda x: json.dumps(x[0]))
 
-combins.saveAsTextFile("%s/results/male/2016031618"  %(HDFS_OUTPUT_PATH) )
+combins.saveAsTextFile("%s/20160315-1618_results_als_female_0311-14_30_003"  %(HDFS_OUTPUT_PATH) )
 
 sc.stop()
