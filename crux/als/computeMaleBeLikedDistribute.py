871b5d957c153d2d6749190d5b3477913a8fe425
oasis2811623 <oasis2811@gmail.com>
2016-03-18 10:01:40 +0800

    update

--- crux/als/computeMaleBeLikedDistribute.py
+++ crux/als/computeMaleBeLikedDistribute.py
@@ -3,10 +3,10 @@ from __init__ import *
 from operator import add
 
 
-data = sc.textFile("%s/results/parameters/female/2016031518_003" % (HDFS_OUTPUT_PATH))
+data = sc.textFile("%s/results/parameters/female/30/2016031418_003_5days" % (HDFS_OUTPUT_PATH))
 
 output=data.map(lambda x:json.loads(x)).filter(lambda x:  x[1][1]>0.6).map(lambda x:(x[0][1],1))\
         .reduceByKey(add).map(lambda x:(x[1],1)).reduceByKey(add)
 
-output.saveAsTextFile("%s/results/maleBeLikedDistribute/2016031518_003_30"  %(HDFS_OUTPUT_PATH) )
+output.saveAsTextFile("%s/results/maleBeLikedDistribute/30/2016031418_003_5days"  %(HDFS_OUTPUT_PATH) )
 sc.stop()
