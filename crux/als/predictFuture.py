271c4d0d283c27e2736adc2589cd069e6c68e9e0
oasis2811623 <oasis2811@gmail.com>
2016-03-18 11:14:35 +0800

    update

--- crux/als/predictFuture.py
+++ crux/als/predictFuture.py
@@ -1,24 +1,15 @@
+import util
 from __init__ import *
 
-data = sc.textFile("%s/data/female/2016031418"  %(HDFS_OUTPUT_PATH) )
+data = sc.textFile("%s/data/female/2016031518"  %(HDFS_OUTPUT_PATH) )
 
-model = MatrixFactorizationModel.load(sc,"%s/model/als_female_parameters/30/als_female_0308-13_003"  %(HDFS_HOME_PATH))
+model = MatrixFactorizationModel.load(sc,"%s/model/als_female_parameters/30/als_female_0311-14_003"  %(HDFS_HOME_PATH))
 
-data2predict=data.map(lambda x:json.loads(x)).filter(lambda x: x[0][0] and x[0][1])\
-    .map(lambda x:((int(x[0][0]), int(x[0][1])),x[1]))
+data2predict=util.prepareData2predict(data)
 data2predict.cache()
 
-#female1=data2predict.map(lambda x:(x[0][0],1)).reduceByKey(lambda x,y:x).count()
-
-#female1 = data2predict.map(lambda x:(x,1)).reduceByKey(lambda x,y:x).count()
-
 #predict the results
-prediction = model.predictAll(data2predict.map(lambda x:x[0])).map(lambda x:((x.user, x.product), x.rating))
-
-
-#female2 = prediction.reduceByKey(lambda x,y:x).count()
-
-#female2= prediction.count()
+prediction = util.predictData(model,data2predict)
 
 #combining with the real results
 combins = data2predict.join(prediction).map(lambda x:(x,1))\
---
+++ crux/als/util.py
@@ -0,0 +1,10 @@
+from __init__ import *
+
+def prepareData2predict(rawData):
+    return rawData.map(lambda x:json.loads(x)).filter(lambda x: x[0][0] and x[0][1])\
+    .map(lambda x:((int(x[0][0]), int(x[0][1])),x[1]))
+
+
+def predictData(model, data2predict):
+
+    return model.predictAll(data2predict.map(lambda x:x[0])).map(lambda x:((x.user, x.product), x.rating))
