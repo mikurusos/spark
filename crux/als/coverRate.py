2b2dade6930eb4222c05611d9eac3b58d0d8090d
oasis2811623 <oasis2811@gmail.com>
2016-03-21 18:54:34 +0800

    update

--- crux/als/coverRate.py
+++ crux/als/coverRate.py
@@ -4,19 +4,25 @@ from operator import add
 rawData=sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016031[3-9]18")
 
 rawData = rawData.map(lambda x:json.loads(x))\
-    .map(lambda x:(int(x[0][0]), int(x[0][1])))\
-    .map(lambda x:(x,1))
+    .map(lambda x:(int(x[0][0]), int(x[0][1]))).cache()
+
+rawMale = rawData.map(lambda x:(x[0],1)).distinct()
+rawFemale = rawData.map(lambda x:(x[1],1)).distinct()
 
 data=sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016032018")
 
 data = data.map(lambda x:json.loads(x))\
     .map(lambda x:(int(x[0][0]), int(x[0][1])))\
-    .map(lambda x:(x,1))
+    .map(lambda x:(x,1)).cache()
 
-total  = data.union(rawData).reduceByKey(add)
+male = rawData.map(lambda x:(x[0],1)).distinct()
+female = rawData.map(lambda x:(x[1],1)).distinct()
 
-num1= total.filter(lambda x:x[1]==2).count()
-num2= data.count()
+num1_male=male.union(rawMale).reduceByKey(add).filter(lambda x:x[1]==1).count()
+num1_female=female.union(rawFemale).reduceByKey(add).filter(lambda x:x[1]==1).count()
+
+num2_male=male.union(rawMale).reduceByKey(add).filter(lambda x:x[1]==2).count()
+num2_female=female.union(rawFemale).reduceByKey(add).filter(lambda x:x[1]==2).count()
 
 with open('/home/hadoop/chen.cheng/Chronos/coverRate', 'w') as f:
-    f.write("%d\t%d\n" %(num1, num2 ) )
+    f.write("%d\t%d\t%d\t%d\n" %(num1_male, num1_female,num2_male,num2_female ) )
