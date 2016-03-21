070bc9ce83f15282673a977194ef91ce1d4935d2
oasis2811623 <oasis2811@gmail.com>
2016-03-21 15:00:03 +0800

    update

--- crux/dataGen.py
+++ crux/dataGen.py
@@ -8,8 +8,9 @@ from config import sc
 
 data = sc.textFile("hdfs://antispam/user/hadoop/output/wang.yuqi/Venus/like_person/%s-24/" %(sys.argv[1]))
 
-out = data.map(lambda x : x.split('\t')).map(lambda x: [json.loads(json.loads(x[0])), json.loads(x[1])[0]])\
-        .map(lambda x: json.dumps(x))
+out = data.map(lambda x : x.split('\t'))\
+        .map(lambda x: [json.loads(json.loads(x[0])), json.loads(x[1])[0]])\
+        .filter(lambda x:x[0][0] and x[0][1] and x[1]).map(lambda x: json.dumps(x))
 
 out.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/rawData/%s-24/"%(sys.argv[1]))
 sc.stop()
