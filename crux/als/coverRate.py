c12fd4d13288f70e2d58ff8d46b2639a9f4ca947
oasis2811623 <oasis2811@gmail.com>
2016-03-23 11:33:10 +0800

    update

--- crux/als/coverRate.py
+++ crux/als/coverRate.py
@@ -1,7 +1,7 @@
 from __init__ import *
 from operator import add
 
-rawData=sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016031[3-9]18")
+rawData=sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016031[0-9]18")
 
 rawData = rawData.map(lambda x:json.loads(x))\
     .map(lambda x:(int(x[0][0]), int(x[0][1]))).cache()
@@ -22,5 +22,5 @@ num1_female=female.count()
 num2_male=male.union(rawMale).reduceByKey(add).filter(lambda x:x[1]==2).count()
 num2_female=female.union(rawFemale).reduceByKey(add).filter(lambda x:x[1]==2).count()
 
-with open('/home/hadoop/chen.cheng/Chronos/coverRate', 'w') as f:
+with open('/home/hadoop/chen.cheng/Chronos/coverRate_10-19_20]', 'w') as f:
     f.write("%d\t%d\t%d\t%d\n" %(num1_male, num1_female,num2_male,num2_female ) )
