fbb3df3dca8a388688617187861335f6555a419e
oasis2811623 <oasis2811@gmail.com>
2016-03-23 16:43:58 +0800

    update

--- crux/als.py
+++ crux/als.py
@@ -12,7 +12,7 @@ sc.setCheckpointDir("hdfs://antispam/user/hadoop/output/chencheng/checkpoint")
 
 #user_artist_data1 = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016030[2-9]18/")
 user_artist_data1 = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016031[0-9]18/")
-user_artist_data2 = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016032[0-1]18/")
+user_artist_data2 = sc.textFile("hdfs://antispam/user/hadoop/output/chencheng/crux/data/female/2016032[0-2]18/")
 
 user_artist_data= user_artist_data1.union(user_artist_data2)
 
@@ -22,10 +22,10 @@ ratings = user_artist_data.map(lambda x: json.loads(x))\
 ratings.checkpoint()
 ratings.cache()
 
-rank = 35
-numIterations = 30
+rank = 30
+numIterations = 25
 
 ALS.checkpointInterval = 2
 model = ALS.train(ratings, rank, numIterations,lambda_=0.03)
 
-model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_female_parameters/35/als_female_0310-21_003_test")
+model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_female_parameters/30/als_female_0310-22_003_test")
