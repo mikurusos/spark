e66e4a529d175c43539de6e40b57fd75974e6836
oasis2811623 <oasis2811@gmail.com>
2016-03-23 14:37:52 +0800

    update

--- crux/als.py
+++ crux/als.py
@@ -22,10 +22,10 @@ ratings = user_artist_data.map(lambda x: json.loads(x))\
 ratings.checkpoint()
 ratings.cache()
 
-rank = 30
-numIterations = 25
+rank = 35
+numIterations = 30
 
 ALS.checkpointInterval = 2
 model = ALS.train(ratings, rank, numIterations,lambda_=0.03)
 
-model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_female_parameters/30/als_female_0310-21_003")
+model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_female_parameters/35/als_female_0310-21_003")
