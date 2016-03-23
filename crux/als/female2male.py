3032c65dc88741d59a537d83bae4c3fb2fa7c614
oasis2811623 <oasis2811@gmail.com>
2016-03-23 17:22:38 +0800

    update

--- crux/als/female2male.py
+++ crux/als/female2male.py
@@ -1,7 +1,7 @@
 import util
 from __init__ import *
 
-female = sc.textFile("%s/data/realdata/chengdu_female0322"  %(HDFS_OUTPUT_PATH) , 12)
+female = sc.textFile("%s/data/realdata/chengdu_female0322"  %(HDFS_OUTPUT_PATH) , 100)
 
 with open("/home/hadoop/data/chengdu_male0322") as f:
     male= [int(i) for i in f.readlines()]
@@ -10,6 +10,6 @@ b = sc.broadcast(male)
 
 result = female.flatMap(lambda x: [(int(x),i) for i in b.value],preservesPartitioning=True)
 
-result.saveAsTextFile("%s/data/realdata/chengdu_result0322"  %(HDFS_OUTPUT_PATH) )
+result.saveAsTextFile("%s/data/realdata/chengdu_result0322_"  %(HDFS_OUTPUT_PATH) )
 
 sc.stop()
