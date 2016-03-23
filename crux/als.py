56313dffc1f90ceb665592ceb421641b4a56bb71
oasis2811623 <oasis2811@gmail.com>
2016-03-23 15:05:39 +0800

    update

--- crux/als.py
+++ crux/als.py
@@ -28,4 +28,4 @@ numIterations = 30
 ALS.checkpointInterval = 2
 model = ALS.train(ratings, rank, numIterations,lambda_=0.03)
 
-model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_female_parameters/35/als_female_0310-21_003")
+model.save(sc,"hdfs://antispam/user/hadoop/output/chencheng/model/als_female_parameters/35/als_female_0310-21_003_test")
