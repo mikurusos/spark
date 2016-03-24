ce296b84e98cc1ca80361580a03fd93497dc55aa
oasis2811623 <oasis2811@gmail.com>
2016-03-24 15:52:17 +0800

    update

--- crux/als/predictFuture.py
+++ crux/als/predictFuture.py
@@ -3,7 +3,7 @@ from __init__ import *
 
 data = sc.textFile("%s/data/male/2016032318"  %(HDFS_OUTPUT_PATH) )
 
-model = MatrixFactorizationModel.load(sc,"%s/model/als_female_parameters/25/als_male_0310-22_003"  %(HDFS_HOME_PATH))
+model = MatrixFactorizationModel.load(sc,"%s/model/als_male_parameters/25/als_male_0310-22_003"  %(HDFS_HOME_PATH))
 
 data2predict=util.prepareData2predict(data)
 data2predict.cache()
