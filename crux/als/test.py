a1ce12651b041448eaad4aee1b49b0b22dea2ec3
oasis2811623 <oasis2811@gmail.com>
2016-03-24 18:26:48 +0800

    update

--- crux/als/test.py
+++ crux/als/test.py
@@ -1,7 +1,7 @@
 from __init__ import *
 
 def getRawData(x):
-    momoid = x['server']["momoid"]
+    momoid = x["momoid"]
     api= x['server']["REQUEST_URI"].split('?')[0]
     return momoid, api
 
@@ -16,6 +16,11 @@ def combineDict(x,y):
 data=sc.textFile("hdfs://antispam/user/flume/events/api/api_logs/2016/01/01/20/api_sink*")
 
 
-data=data.map(lambda x: json.loads(x)).map(getRawData).map(lambda x: (x[0],  {x[1]:1} ))\
+data=data.map(lambda x: json.loads(x)).filter(lambda x:x["momoid"])\
+    .map(getRawData).map(lambda x: (x[0],  {x[1]:1} ))\
     .reduceByKey(combineDict).map(lambda x:json.dumps(x))
+
+
+data.saveAsTextFile("hdfs://antispam/user/hadoop/output/chencheng/test")
+sc.stop()
 
