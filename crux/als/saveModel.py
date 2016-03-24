9fe56a7ce7a42e5eeb0bd3d8cc23e0be29bcc4ef
oasis2811623 <oasis2811@gmail.com>
2016-03-24 11:44:13 +0800

    update

--- crux/als/saveModel.py
+++ crux/als/saveModel.py
@@ -1,15 +1,14 @@
 from __init__ import *
 
-model = MatrixFactorizationModel.load(sc,"%s/model/als_female_parameters/30/als_female_0310-21_003"  %(HDFS_HOME_PATH))
+model = MatrixFactorizationModel.load(sc,"%s/model/als_female_parameters/30/als_female_0310-22_003"  %(HDFS_HOME_PATH))
 
 female= model.userFeatures().map(lambda x:(x[0],list(x[1]))).map(lambda x:json.dumps(x))
 
-female.saveAsTextFile("%s/data/realdata/0322/model_0310-21_003_female"  %(HDFS_OUTPUT_PATH) )
+female.saveAsTextFile("%s/data/realdata/0322/model_0310-22_003_female"  %(HDFS_OUTPUT_PATH) )
 
 male= model.productFeatures().map(lambda x:(x[0],list(x[1]))).map(lambda x:json.dumps(x))
 
-male.saveAsTextFile("%s/data/realdata/0322/model_0310-21_003_male"  %(HDFS_OUTPUT_PATH) )
-
+male.saveAsTextFile("%s/data/realdata/0322/model_0310-22_003_male"  %(HDFS_OUTPUT_PATH) )
 
 sc.stop()
 
